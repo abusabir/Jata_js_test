da1e5fdc0f54cbb2b82c9fdd7a7b348f6bc8f9a9
abusabir <oleg.agapitov@gmail.com>
2015-12-12 19:48:59 +0200

    the routing is working

--- js/map.js
+++ js/map.js
@@ -41,7 +41,6 @@ class PlaceMark {
     }
 
     clear() {
-        //debugger;
         if(!this.index && this.index != 0) return;
         this.collection.remove(this.collection.get(this.index));
         this.index = null;
@@ -50,23 +49,32 @@ class PlaceMark {
 
 
 class Router {
+
     constructor(options) {
         this.map = options.map;
         this.pointA = options.pointA;
         this.pointB = options.pointB;
-        this.interPoints = options.points;
         this.collection = this.map.geoObjects;
-
+        this._startEditing = false;
+        this._isButtonListening = false;
         this.render();
     }
 
     render() {
         let ctx = this;
+        let coords = [];
 
-        let start_coords = this.pointA.geometry.getCoordinates();
-        let end_coords = this.pointB.geometry.getCoordinates();
+        if(this.points) {
+            this.points.each(function (elem, i) {
+                coords[i] = elem.geometry.getCoordinates();
+            });
 
-        ymaps.route([start_coords, end_coords])
+        } else {
+            coords.push(this.pointA.geometry.getCoordinates());
+            coords.push(this.pointB.geometry.getCoordinates());
+        }
+
+        ymaps.route(coords)
             .then(
                 function(route) {
                     ctx.route = route;
@@ -80,32 +88,48 @@ class Router {
     }
 
     add() {
+        let editButton = document.getElementById('editor');
+        let ctx = this;
+
         this.collection.add(this.route);
 
-        let points = this.route.getWayPoints(), lastPoint = points.getLength() - 1;
-        points.options.set('preset', 'islands#redStretchyIcon');
-        points.options.set('draggable', 'true');
-        points.get(0).properties.set('iconContent', 'A');
-        points.get(lastPoint).properties.set('iconContent', 'Б');
+        if(!this._isButtonListening) {
+            editButton.addEventListener('click', () => {
+                if (this._startEditing = !this._startEditing) {
+                    this.route.editor.start({ addWayPoints: true });
+                    editButton.value = 'Отключить редактор маршрута';
+                } else {
+                    this.route.editor.stop();
+                    editButton.value = 'Включить редактор маршрута';
+                }
+            });
+            this._isButtonListening = true;
+        }
 
-        this.pointA = points.get(0);
-        this.pointB = points.get(lastPoint);
+        this.points = this.route.getWayPoints();
+        let lastPoint = this.points.getLength() - 1;
 
-        console.log('ok');
+        this.points.options.set('preset', 'islands#redStretchyIcon');
+        this.points.options.set('draggable', 'true');
 
-        this.pointA.events.add('dragend', (e) => this.onChange(e));
-        this.pointB.events.add('dragend', (e) => this.onChange(e));
+        //this.pointA = this.points.get(0);
+        //this.pointB = this.points.get(lastPoint);
+
+        this.points.each( function(elem) {
+            elem.events.add('dragend', (e) => ctx.onChange(e));
+        });
+
 
     }
 
     onChange(e) {
         let thisPoint = e.get('target');
         let coords = thisPoint.geometry.getCoordinates();
-
         thisPoint.geometry.setCoordinates(coords);
-
         this.render();
     }
+
+
 
 }
 
@@ -121,40 +145,45 @@ function init() {
         searchControlProvider: 'yandex#search'
     });
 
+
+
     let start_elem = document.getElementById('ts_route_start');
     let end_elem = document.getElementById('ts_route_finish');
+    let isRouterInitialized = false;
 
-    let points = new ymaps.GeoObjectCollection();
 
     let startP = new PlaceMark({
         map: myMap,
         elem: start_elem,
-        content: 'A',
-        collection: points
+        content: 'A'
     });
 
     let endP = new PlaceMark({
         map: myMap,
         elem: end_elem,
-        content: 'B',
-        collection: points
+        content: 'B'
     });
 
-    myMap.geoObjects.events.add('add', (e) => createRouter(e, myMap, startP, endP));
+    myMap.geoObjects.events.add('add', (e) => {
+        if(isRouterInitialized) return;
+        isRouterInitialized = createRouter(e, myMap, startP, endP);
+    });
 }
 
 
 function createRouter(e, map, pointA, pointB, ...interPoints) {
     let target = e.get('target');
     let last_added = target.get(target.getLength() - 1);
-    if(!last_added.geometry) return;
+    if(!last_added.geometry) return false;
 
-    if(!pointA.coords || !pointB.coords) return;
+    if(!pointA.coords || !pointB.coords) return false;
 
     let router = new Router({
         map: map,
         pointA: pointA.placemark,
         pointB: pointB.placemark
     });
+
+    return true;
 
 }
